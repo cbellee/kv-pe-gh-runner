343d03c8326ee5e98879377b774c2f2321d5ba47
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2023-06-26 10:47:17 +1000

    fixed blob container in TF v5

--- iac/main.tf
+++ iac/main.tf
@@ -140,7 +140,7 @@ resource "random_string" "random" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "${var.location}${random_string.random.result}"
+  name                     = lower("${var.location}${random_string.random.result}")
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
